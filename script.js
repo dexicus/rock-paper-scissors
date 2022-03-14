99b29126d169dd61e27d410fd6c090ed1e0fb3c4
dexicus <tjpartridge@live.co.uk>
2022-03-14 15:44:06 +0000

    Add UI functionality to the game

--- script.js
+++ script.js
@@ -7,63 +7,83 @@ function computerPlay() {
 
 //Compare values to determine winner of a single round
 function playRound(player, computer) {
-    playerSelection = prompt('Choose rock paper or scissors...').toLowerCase();
     computerSelection = computerSelection.toLowerCase();
     if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' &&
             computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-        console.log(`You won - ${playerSelection} beats ${computerSelection}.`);
+        roundResult.textContent = (`You won - ${playerSelection} beats ${computerSelection}.`);
         return result = 'playerWin';
     }
     else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' &&
             computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'rock')) {
-        console.log(`You lost - ${computerSelection} beats ${playerSelection}.`);
+        roundResult.textContent = (`You lost - ${computerSelection} beats ${playerSelection}.`);
         return result = 'computerWin';
     }
     else if ((playerSelection === 'rock' && computerSelection === 'rock') || (playerSelection === 'paper' &&
             computerSelection === 'paper') || (playerSelection === 'scissors' && computerSelection === 'scissors')) {
-        console.log('You drew.');
+        roundResult.textContent = ('You drew.');
         return result = 'draw';
     }
 }
 
-//Repeat 5x for a full game
-function playFullGame() {
-    for (let i = 0; i < 5; ++i) {
-        playRound(playerSelection, computerSelection);
-        if (result === 'playerWin') {
-            ++playerWinCount
-        }
-        else if (result === 'computerWin') {
-            ++computerWinCount
-        }
-        else if (result === 'draw') {
-            ++drawCount
-        }
-        console.log(`Computer chose ${computerSelection}. Player: ${playerWinCount} Computer: ${computerWinCount}`);
-        computerSelection = computerPlay();
-        if (playerWinCount === 3 || computerWinCount === 3) {
-            return
-        }
+//First to 5 wins, compare wins to detemine overall winner
+function printWhoWon() {
+    if (playerWinCount === 5 && playerWinCount > computerWinCount) {
+        buttons.forEach((button) => {
+            button.disabled = true
+        });
+        winner.textContent = 'You won.'
+        createResetButton();
+        return
     }
+    else if (computerWinCount === 5 && playerWinCount < computerWinCount){
+        console.log('Computer wins.');
+        buttons.forEach((button) => {
+            button.disabled = true
+        });
+        winner.textContent = 'Computer wins.'
+        createResetButton();
+        return
+    }
+    else return
 }
 
-//Compare wins to detemine overall winner
-function printWhoWon() {
-    if (playerWinCount > computerWinCount) {
-        console.log('Player wins.');
-    }
-    else if (playerWinCount < computerWinCount){
-        console.log('Computer wins.');
-    }
-    else if (playerWinCount === computerWinCount) {
-        console.log('The game is a draw.');
-    }
+//Button choice function
+function chooseRpsOption () {
+    buttons.forEach((button) => {
+        button.addEventListener('click', function() {
+            playerSelection = button.textContent.toLowerCase();
+            playRound(playerSelection, computerSelection);
+            if (result === 'playerWin') {
+                ++playerWinCount
+            }
+            else if (result === 'computerWin') {
+                ++computerWinCount
+            }
+            else if (result === 'draw') {
+                ++drawCount
+            }
+            roundResult.textContent = `Computer chose ${computerSelection}. Player: ${playerWinCount} Computer: ${computerWinCount}`;
+            computerSelection = computerPlay();
+            printWhoWon();
+            return
+        });
+    });
 }
+
+function createResetButton() {
+    resetButton.classList.add('reset');
+    resetButton.textContent = 'Reset';
+    score.appendChild(resetButton);
+    resetButton.addEventListener('click', () => {
+        window.location.reload()
+    });
+}
+
 
 function main() {
-    playFullGame();
-    printWhoWon();
+    chooseRpsOption();
 }
+
 
 //Take player & computer choice, assign to a variable
 let playerSelection = '';
@@ -71,9 +91,18 @@ let computerSelection = computerPlay();
 
 
 //Track who wins how many games, and how many are draws
+let result = '';
 let playerWinCount = 0;
 let computerWinCount = 0;
 let drawCount = 0;
+
+const buttons = document.querySelectorAll('button');
+const roundResult = document.querySelector('.round-result');
+const winner = document.querySelector('.winner');
+
+//Reset button
+const score = document.querySelector('.score');
+const resetButton = document.createElement('button');
 
 
 main();
